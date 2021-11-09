88f7741f77034ebb4692c370c3ea3d1409d66f7e
Vladlen <adment@b1oga.net>
2021-11-09 18:25:34 +0200

    server

--- src/Server.java
+++ src/Server.java
@@ -2,8 +2,8 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.InputStreamReader;
 import java.io.InputStream;
-import java.io.BufferedReader;
 import java.io.OutputStream;
+import java.io.BufferedReader;
 
 public class Server {
 
